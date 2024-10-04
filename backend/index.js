f19df697559b4ebaa97891d230cea3f773dc1043
RajaXSharma <rajasharma844891@gmail.com>
2024-10-04 23:41:46 +0530

    full

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+const { createServer } = require("node:http");
+const cors = require("cors");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("<h1>hello</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("newMessage", ({username, message}) => {
+    console.log(username, message);
+    io.emit("newMessage", { username, message });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+server.listen(4000, () => {
+  console.log("server running at http://localhost:4000");
+});
